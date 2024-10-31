0d3f41973e4dec114c1258f9c0ef110ce8d6251a
liamhayden <liam.hayden.code@gmail.com>
2024-10-31 11:27:38 +0000

    Updated PrinterController. - Needs to be tested.

--- src/main/java/com/example/Printers/Printer/PrinterController.java
+++ src/main/java/com/example/Printers/Printer/PrinterController.java
@@ -1,5 +1,11 @@
 package com.example.Printers.Printer;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Printers")
 public class PrinterController {
 
     private final PrinterService printerService;
@@ -9,13 +15,32 @@ public class PrinterController {
     }
 
     // create
-
+    @PostMapping("/new")
+    public Printer createPrinter(@ModelAttribute Printer printer) {
+        return printerService.createPrinter(printer);
+    }
     // read
+    @GetMapping("/{id}")
+    public Printer getPrinter(@PathVariable int id) {
+        return printerService.getPrinter(id);
+    }
 
     // read all
+    @GetMapping("/all")
+    public List<Printer> getAllPrinters() {
+        return printerService.getAllPrinters();
+    }
 
     // update
+    @PutMapping("/{id}")
+    public Printer updatePrinter(@PathVariable int id, @ModelAttribute Printer printer) {
+        return printerService.updatePrinter(printer, id);
+    }
 
     // delete
+    @DeleteMapping("/{id}")
+    public String deletePrinter(@PathVariable int id) {
+        return printerService.deletePrinter(id);
+    }
 
 }
